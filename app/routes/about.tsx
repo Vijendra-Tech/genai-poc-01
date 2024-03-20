da280d746d20097b4a2b8a3eab8609639deddc14
Vijendra Rana <vkrana@192.168.1.6>
2024-03-20 20:02:06 +0530

    3d logo added

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -7,12 +7,13 @@ function About() {
     return (
         <WrappedComponent heading=''>
             <div className='flex flex-col justify-start gap-2 mt-32 '>
-                <Card className="w-[350px] bg-white h-[200px]">
+                <Card className="w-[700px] bg-white h-[500px]">
                     <CardContent>
+                        <img src="../../profile.png" alt="AI" className="w-72 h-72 bg-white" />
                         <div className='grid w-full items-center gap-4 ml-10 mt-10'>
-                        <H3>Vijendra Rana</H3>
-                        <p>Associate Consultant</p>
-                        <p>T2.Fusion</p>
+                            <H3>Vijendra Rana</H3>
+                            <p>Associate Consultant</p>
+                            <p>T2.Fusion</p>
                         </div>
                     </CardContent>
                 </Card>
